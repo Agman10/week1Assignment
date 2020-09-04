7d78baa4cfd716522972c61098aab2cfbf3ffb40
August Möller <august.moller@futuregames.nu>
2020-09-04 12:38:56 +0200

    added particle and sound

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
+﻿using System.CodeDom.Compiler;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,12 +14,23 @@ public class PlayerController : MonoBehaviour
 
     private float xSpeed, zSpeed;
 
+    
+    public Text scoreText;
     public int score;
     public int gemValue = 1;
+
+    public AudioSource gemSound;
+    
+
     void Start()
     {
         moveSpeed = 8f;
         rb = GetComponent<Rigidbody>();
+
+        score = 0;
+        scoreText.text = "Gems: " + score.ToString();
+
+        gemSound = GameObject.FindWithTag("GemSound").GetComponent<AudioSource>();
     }
 
     void FixedUpdate()
@@ -36,9 +49,11 @@ public class PlayerController : MonoBehaviour
     private void OnTriggerEnter(Collider Other)
     {
         if (Other.gameObject.CompareTag("Gem")){
+            gemSound.Play();
             Destroy(Other.gameObject);
-            ScoreManager.instance.ChangeScore(gemValue);
-            score++;
+            //ScoreManager.instance.ChangeScore(gemValue);
+            score = score + 1;
+            scoreText.text = "Gems: " + score.ToString();
         }
     }
 }
---
+++ Assets/scripts/SoundPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+    public AudioSource source;
+
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        //source.Play(0);
+        
+    }
+    void OnTriggerEnter()
+    {
+        source.Play();
+    }
+}
